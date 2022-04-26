0f3170d8cf7b9e9335050ccf67749e4e6fbda681
VincentMeziane <mezianev@hotmail.fr>
2022-04-26 18:32:23 +0200

    Feature : Recherche liste achievements côté user

--- assets/javascript/muuri_grid.js
+++ assets/javascript/muuri_grid.js
@@ -1,3 +1,4 @@
+import debounce from 'debounce';
 import Muuri from 'muuri';
 
 if (document.getElementsByClassName('grid').length !== 0) {
@@ -5,6 +6,15 @@ if (document.getElementsByClassName('grid').length !== 0) {
     var ownedFilter = document.getElementById('grid-filter-owned');
     var notOwnedFilter = document.getElementById('grid-filter-not-owned');
     var noFilter = document.getElementById('grid-filter-none');
+    var searchFilter = document.getElementById('grid-filter-search');
+
+    const debouncedSearch = debounce((e) => {
+        const query = e.target.value;
+        grid.filter((item) => {
+            const text = item._element.innerText
+            return text.includes(query);
+        })
+    }, 200);
     
     ownedFilter.addEventListener('click', () => {
         grid.filter('[data-owned]');
@@ -15,4 +25,6 @@ if (document.getElementsByClassName('grid').length !== 0) {
     noFilter.addEventListener('click', () => {
         grid.filter((item) => {return true});
     })
+
+    searchFilter.addEventListener('keyup', (e) => debouncedSearch(e));
 }
